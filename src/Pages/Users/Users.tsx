2397a304a0a6708ac3d9932afd97f90979e18699
harshlodhi00 <harsh.lodhi.19@iitram.ac.in>
2023-10-27 09:59:43 +0530

    User Page Begins 4

--- src/Pages/Users/Users.tsx
+++ src/Pages/Users/Users.tsx
@@ -5,9 +5,15 @@ const Users = () => {
     <div className="users">
       <div className="info">
         <h1>Users</h1>
+        <button className="Button">Add New User</button>
+        <button className="Button">Add New User</button>
+        <button className="Button">Add New User</button>
+        <button className="Button">Add New User</button>
+        <button className="Button">Add New User</button>
+        <button>Add New User</button>
+        <button>Add New User</button>
         <button>Add New User</button>
       </div>
-      
     </div>
   );
 };
